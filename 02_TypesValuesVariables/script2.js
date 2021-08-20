bea4c186ae6c683349efdf0d685a6801baaa6665
so <S.phairoh20dec@gmail.com>
2021-08-20 13:50:20 +0700

    week2

--- 02_TypesValuesVariables/script2.js
+++ 02_TypesValuesVariables/script2.js
@@ -5,7 +5,7 @@
 let num = 5;
 let Num = 'This is not a number';
 console.log('num is ' + num);
-console.log('Num is' + Num);
+console.log('Num is ' + Num);
 
 //Multiple statements can be combined into a code block by using {}
 //require code block only when executing multiple statements.
@@ -34,4 +34,39 @@ console.log(`type of mySymbol is ${typeof mySymbol}`);
 let myNull = null;
 console.log(`type of myNull is ${typeof myNull}`);
 
-console.log('Good bye!');
+let myArr = [1, 2, 3];
+
+console.log(`myArr Length: ${myArr.length}`);
+console.log(`type of myArr is ${typeof myArr}`);
+
+let myObj = {
+  id: 1,
+  task: 'grading exam'
+};
+
+// {
+//   id: 2,
+//   task: 'class lecture',
+//   date: new Date()
+// }
+let myObj2 = {
+  id: 1,
+  task: 'grading exam'
+};
+console.log(`${JSON.stringify(myObj)}`);
+//The JSON.stringify() method converts a JavaScript object or value to a JSON string
+console.log(`type of myObj is ${typeof myObj}`);
+
+newObj = myObj;
+console.log(`${JSON.stringify(newObj)}`);
+console.log(`type of newObj is ${typeof newObj}`);
+
+console.log(`newObj === myObj is ${newObj === myObj}`);
+console.log(`myObj1 === myObj2 is ${myObj === myObj2}`);
+
+let a = [];
+let b = a;
+b[0] = 1;
+let c = [1];
+console.log(`a === b is ${a === b}`);
+console.log(`b == c is ${b == c}`);
