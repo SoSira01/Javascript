80053dad5f98aaccc8f1c8d9e5f88531694656ce
so <S.phairoh20dec@gmail.com>
2021-12-07 22:04:15 +0700

    local storage

--- Practice/Example2/Local Storage/script.js
+++ Practice/Example2/Local Storage/script.js
@@ -1,13 +1,25 @@
+let clicker = document.getElementById('click');
+let deleteclicker = document.getElementById('deleteclick');
 
 function clickCounter() {
-  if (typeof(Storage) !== "undefined") {
-    if (localStorage.clickcount) {
-      localStorage.clickcount = Number(localStorage.clickcount)+1;
-    } else {
-      localStorage.clickcount = 1;
-    }
-    document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
-  } else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+      if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+      } else {
+        localStorage.clickcount = 1;
+      }
+      const results = document.getElementById("result");
+      results.innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+
   }
+
+  function clickDelete() {
+    if (localStorage.clickcount) {
+        localStorage.clickcount = 0;
+    } 
+    
+    const results = document.getElementById("result");
+    results.innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
 }
+
+clicker.addEventListener('click',clickCounter);
+deleteclicker.addEventListener('click',clickDelete);
